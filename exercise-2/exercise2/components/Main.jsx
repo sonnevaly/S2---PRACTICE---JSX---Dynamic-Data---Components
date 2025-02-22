4404efa09ed3267fc67f3fb3a8e4e06eb8c74cf8
Heng Hakley <Hakleybtb18kh@gmail.com>
2025-02-22 20:58:27 +0700

    Create Main.jsx

---
+++ exercise-2/exercise2/components/Main.jsx
@@ -0,0 +1,9 @@
+function main(){
+    return (
+            <main>
+                <h1>Exercise 2</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis aperiam error repellendus reprehenderit non, rerum recusandae excepturi alias officiis. Quasi quos quaerat accusantium ipsa. Sed in ipsum aperiam deleniti repudiandae.</p>
+            </main>
+    );
+}
+export default main;
